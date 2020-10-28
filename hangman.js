e634cfcc2e47f0dbdc17f120a5d1fbcdaec41138
Prince Shokoya <72402497+PS-Development@users.noreply.github.com>
2020-10-28 18:40:23 +0000

    Add files via upload

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 const Hangman = function(word, remGuess) {
     this.word = word.toLowerCase().split('')
     this.remGuess = remGuess
-    this.guessedLetters = ['c', 'e']
+    this.guessedLetters = []
 }
 
 Hangman.prototype.getPuzzle = function() {
@@ -17,8 +17,17 @@ Hangman.prototype.getPuzzle = function() {
     return puzzle
 }
 
-const game1 = new Hangman ('Cat', 2)
-console.log(game1.getPuzzle())
+Hangman.prototype.makeGuess = function(guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const badGuess = !this.word.includes(guess)
 
-const game2 = new Hangman ('New Jersey', 4)
-console.log(game2.getPuzzle())
+    if (isUnique) {
+        this.guessedLetters.push(guess)
+    }
+
+    if (isUnique && badGuess) {
+        this.remGuess--
+    }
+}
+
